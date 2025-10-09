961a73bb968a0d02ab62fa1baa33a04b85a6a735
luciakusumaw <itsmelucia27@gmail.com>
2025-10-09 15:04:46 +0700

    task 2

---
+++ jobsheet5/Task2.java
@@ -0,0 +1,17 @@
+package jobsheet5;
+import java.util.Scanner;
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int number;
+
+        System.out.println("Input Te Number: ");
+        number = sc.nextInt();
+        if (number % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    } 
+}
