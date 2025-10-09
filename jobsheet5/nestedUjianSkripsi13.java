e97d7a605225e1a2aeee9693ec90410691d13df3
luciakusumaw <itsmelucia27@gmail.com>
2025-10-09 13:57:48 +0700

    attempt 3 (2)

--- jobsheet5/nestedUjianSkripsi13.java
+++ jobsheet5/nestedUjianSkripsi13.java
@@ -3,30 +3,32 @@ import java.util.Scanner;
 public class nestedUjianSkripsi13 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String message;
-        System.out.print("Has the student been cleared of compensation? (Yes/No): ");
+        System.out.print("Has the student been cleared of compensation? (Y/T or Yes/No): ");
         String compensationStatus = sc.nextLine().trim();
-        //trim() function is used to remove spaces before and after a String.
+
         System.out.print("Enter the number of guidance logs with Supervisor 1: ");
-        int supervisor1 = sc.nectInt();
+        int supervisor1 = sc.nextInt();
+
         System.out.print("Enter the number of guidance logs with Supervisor 2: ");
-        int supervisor2 = sc.nectInt();
+        int supervisor2 = sc.nextInt();
 
-        if (free) {
+        String message;
+
+        if (compensationStatus.equalsIgnoreCase("Y") || compensationStatus.equalsIgnoreCase("Yes")) {
             if (supervisor1 >= 8 && supervisor2 >= 4) {
-                message = "All requirements are met. The student may register for the thesis exam.";
-            } else if (supervisor1<8 && supervisor2<4) {
-                message = "Failed: Guidance logs with Supervisor 1 are fewer than 8 AND Supervisor 2 fewer than 4";
-            }
-            else if (supervisor1 < 8 && supervisor2 >= 4) {
-                message = "Failed: Guidance logs with Supervisor 1 are fewer than 8.";
-            } else if (supervisor1 >= 8 && supervisor2 < 4) {
-                message = "Failed: Guidance logs with Supervisor 2 are fewer than 4.";
-            }
+        message = "All requirements are met. The student may register for the thesis exam.";
+        } else if (supervisor1 < 8 && supervisor2 < 4) {
+        message = "Failed: Guidance logs with Supervisor 1 are fewer than 8 AND Supervisor 2 fewer than 4.";
+        } else if (supervisor1 < 8) {
+        message = "Failed: Guidance logs with Supervisor 1 are fewer than 8.";
         } else {
-            message = "Failed: The student has not been cleared of compensation.";
+        message = "Failed: Guidance logs with Supervisor 2 are fewer than 4.";
         }
+        } else {
+    message = "Failed: The student has not been cleared of compensation.";
+}
 
+        System.out.println(message);
     }
 }
     
