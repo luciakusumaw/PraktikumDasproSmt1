8bc89adb030739231830cfcfff75276377ddd952
luciakusumaw <itsmelucia27@gmail.com>
2025-10-09 12:28:38 +0700

    modifiation attempt 2

---
+++ jobsheet5/ifElsePrintKRS13.java
@@ -0,0 +1,29 @@
+package jobsheet5;
+import java.util.Scanner;
+public class ifElsePrintKRS13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("--- Print KRS SIAKAD ---");
+        System.out.print("Enter current semester: ");
+        int semester = sc.nextInt();
+
+        if (semester == 1) {
+            System.out.println("KRS semester 1 is displayed");
+        } else if (semester == 2) {
+            System.out.println("KRS semester 2 is displayed");
+        } else if (semester == 3) {
+            System.out.println("KRS semester 3 is displayed");
+        } else if (semester == 4) {
+            System.out.println("KRS semester 4 is displayed");
+        } else if (semester == 5) {
+            System.out.println("KRS semester 6 is displayed");
+        } else if (semester == 6) {
+            System.out.println("KRS semester 6 is displayed");
+        } else if (semester == 7) {
+            System.out.println("KRS semester 7 is displayed");
+        } else if (semester == 8) {
+            System.out.println("KRS semester 8 is displayed");
+        }
+        sc.closse();
+    }   
+}
