f55eb5f17f74ebe6c146ab13b36508c0ad7266d6
luciakusumaw <itsmelucia27@gmail.com>
2025-10-09 11:43:19 +0700

    1st try

---
+++ jobsheet5/ifPrintKRS13.java
@@ -0,0 +1,15 @@
+package jobsheet5;
+import java.util.Scanner;
+public class ifPrintKRS13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("--- Print KRS SIAKAD ---");
+        System.out.print("Has UKT been paid? (True/False): ");
+        boolean uktPaid = sc.nextBoolean();
+
+        if (uktPaid) {
+            System.out.println("UKT payment verified");
+            System.out.println("Print your KRS and ask the DPA signature");
+        }
+    }
+}
